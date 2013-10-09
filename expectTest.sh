6a769dc48a01064e87ad60fd673fb9780912f6f4
mrbig <466715750@qq.com>
2013-10-10 02:29:27 +0800

    fix

---
+++ expectTest.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/expect -f
+send "please input root account name\n"
+expect "root"
+set account [string trim $expect_out(buffer)]
+send "input passwd: \n"
+expect "123456"
+set passwd [string trim $expect_out(buffer)]
+spawn echo p$passwd
+spawn mysql -u$account -p$passwd
+#spawn ssh mrbiglule@lule.me
+send "use naladb\n"
+send "show tables;\n"
+interact
